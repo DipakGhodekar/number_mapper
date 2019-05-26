c8052a70db66256768567d8aaef2ca79a684e2ca
Dipak Ghodekar <dghodekar1@gmail.com>
2019-05-26 15:29:58 +0530

    Configure digit with alphabets

--- lib/number_mapper.rb
+++ lib/number_mapper.rb
@@ -1,2 +1,8 @@
+require 'yaml'
+
 class NumberMapper
+  # Maping of number with letters
+  def digit_mapping
+    @digit_mapping ||= YAML.safe_load(File.read("#{__dir__}/digit_mapping.yml"))
+  end
 end
