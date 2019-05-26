b70557834b393405f1e8069484efe9caf730b7f7
Dipak Ghodekar <dghodekar1@gmail.com>
2019-05-26 15:56:14 +0530

    Map number to word combinations   # 1. Map number to set of characters   # 2. Check this set of character is word   # 2.1 If yes, then mark it as word   # 2.1.1 continue same process with remaining digits and searched word   # 2.1.2 continue same process with remaining digits and without considering searched word as word   # 2.2 If no, continue same process with remaining digits

--- lib/number_mapper.rb
+++ lib/number_mapper.rb
@@ -1,6 +1,14 @@
 require 'yaml'
 
 class NumberMapper
+  attr_accessor :word_combinations
+
+  def search_word_combinations(number)
+    self.word_combinations = []
+    search_words(number.to_s)
+    word_combinations
+  end
+
   def word_exists?(word)
     word.upcase!
     searched_word = dictionary_words.bsearch do |dictionary_word|
@@ -9,12 +17,30 @@ class NumberMapper
     searched_word == word
   end
 
+  private
+
+  def search_words(number, words = '')
+    digit = number[0].to_i
+    number = number[1..-1]
+    digit_mapping[digit].each do |char|
+      new_words = words + char
+      last_word = new_words.split(',').last
+      if (last_word.length > 2) && word_exists?(last_word)
+        if number.empty?
+          word_combinations << new_words
+        else
+          search_words(number, new_words) unless number.empty?
+          new_words += ','
+        end
+      end
+      search_words(number, new_words) unless number.empty?
+    end
+  end
+
   # Maping of number with letters
   def digit_mapping
     @digit_mapping ||= YAML.safe_load(File.read("#{__dir__}/digit_mapping.yml"))
   end
-
-  private
 
   def dictionary_words
     @dictionary_words ||= File.readlines('lib/dictionary.txt').map(&:strip)
--- spec/number_mapper_spec.rb
+++ spec/number_mapper_spec.rb
@@ -4,6 +4,29 @@ RSpec.describe NumberMapper do
   let(:subject) { NumberMapper.new }
 
   describe 'Instance Methods' do
+    describe '#search_word_combinations' do
+      it 'returns possible combinations of words for gives number' do
+        expect(
+          subject.search_word_combinations(6_686_787_825)
+        ).to include(
+          'MOTORTRUCK',
+          'MOTOR,TRUCK',
+          'MOTOR,USUAL',
+          'NOUN,STRUCK',
+          'NOT,OPT,PUCK'
+        )
+
+        expect(
+          NumberMapper.new.search_word_combinations(2_282_668_687)
+        ).to include(
+          'CATAMOUNTS',
+          'ACTA,MOUNTS',
+          'ACT,AMOUNTS',
+          'ACT,CONTOUR',
+          'CAT,BOOT,OUR'
+        )
+      end
+    end
     describe '#word_exists?' do
       context 'word exists in dictionary' do
         context 'when word have mixed case characters' do
